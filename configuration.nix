6b32460eddbf1f2662e3844710849b9edb6f0c89
div72 <noreply@div72.xyz>
2023-11-30 22:05:03 +0000

    all: initial commit

---
+++ configuration.nix
@@ -0,0 +1,85 @@
+{ config, libs, pkgs, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+    ./networking.nix # generated at runtime by nixos-infect  
+  ];
+
+  system.stateVersion = "23.05";
+
+  boot.cleanTmpDir = true;
+  zramSwap.enable = true;
+
+  networking.hostName = "hacettepeoyt-vflower";
+  networking.domain = "";
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 80 25565 ];
+
+  programs.mosh.enable = true;
+  programs.zsh.enable = true;
+
+  services.openssh.enable = true;
+
+  services.nginx = {
+    enable = true;
+  };
+
+  systemd.services.minecraft-server = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    startLimitBurst = 3;
+    startLimitIntervalSec = 60;
+
+    serviceConfig = {
+      ExecStart = "/etc/profiles/per-user/minecraft/bin/minecraft-server";
+
+      Restart = "always";
+      WorkingDirectory = "/srv/minecraft";
+      User = "minecraft";
+      Group = "minecraft";
+      Type = "simple";
+
+      LockPersonality = true;
+      PrivateDevices = true;
+      PrivateTmp = true;
+      PrivateUsers = true;
+      ProtectClock = true;
+      ProtectControlGroups = true;
+      ProtectHome = true;
+      ProtectHostname = true;
+      ProtectKernelLogs = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      ProtectProc = "invisible";
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+      SystemCallArchitectures = "native";
+      UMask = "0007";
+    };
+  };
+
+  users.mutableUsers = false;
+  users.users.root.openssh.authorizedKeys.keys = [ ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVb2l/23ykDnfhO5VrkCQaycfF9oCo1Jig/JeG86w//'' ];
+  users.users = {
+    div72 = {
+      isNormalUser = true;
+      passwordFile = "/etc/secrets/passwd/div72";
+      shell = pkgs.zsh;
+      extraGroups = [ "wheel" ];
+      packages = [ pkgs.git pkgs.tmux pkgs.vim ];
+      openssh.authorizedKeys.keys = [ ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVb2l/23ykDnfhO5VrkCQaycfF9oCo1Jig/JeG86w//'' ];
+    };
+
+    minecraft = {
+      isSystemUser = true;
+      home = "/srv/minecraft";
+      createHome = true;
+      homeMode = "770";
+      group = "minecraft";
+      packages = [ pkgs.papermc ];
+    };
+  };
+
+  users.groups.minecraft = { };
+}
