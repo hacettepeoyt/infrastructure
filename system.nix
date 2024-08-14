a238ca35b82e44705e97461a2ffe71ca7a3dfdb8
furkansimsekli <simseklifurkan0@gmail.com>
2024-08-14 21:00:48 +0300

    users: add shell script bump-announcement-bot

--- system.nix
+++ system.nix
@@ -221,6 +221,34 @@
           git commit -m "services: bump hu-cafeteria-bot to $1"
           git push
         '')
+
+        (pkgs.writeShellScriptBin "bump-announcement-bot" ''
+          cd /etc/nixos
+
+          set -e
+
+          if [ ! -z "$(git status --porcelain)" ]; then
+            echo "Somebody forgot to commit the changes! Exiting."
+            exit 1
+          fi
+
+          if [ -z "$SSH_AUTH_SOCK" ]; then
+            echo "You need to forward your SSH agent. (ssh -A ...)"
+            exit 1
+          fi
+
+          sudo chgrp -R wheel /etc/nixos
+          sudo chown -R g+w /etc/nixos
+
+          new_hash=$(nix-prefetch fetchFromGitHub --owner hacettepeoyt --repo hu-announcement-bot --rev "v$1")
+          sed -i -r -e 's|hash = "sha256-[a-zA-Z0-9/=]+";|hash = "'"$new_hash"'";|g' /etc/nixos/services/hu-announcement-bot.nix
+          sed -i -r -e 's|version = "[0-9.-]+";|version = "'"$1"'";|g' /etc/nixos/services/hu-announcement-bot.nix
+
+          sudo nixos-rebuild switch
+          git add /etc/nixos/services/hu-announcement-bot.nix
+          git commit -m "services: bump hu-announcement-bot to $1"
+          git push
+        '')
       ];
       openssh.authorizedKeys.keys = [ ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKjtQ/SbNBCTSWimPetOw4veFxXANwPNdprjFiEQa2O'' ];
     };
