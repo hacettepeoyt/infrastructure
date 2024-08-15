fa811f3dbcfd19a7dda2fd99741b27eb5054e3f7
div72 <noreply@div72.xyz>
2024-08-15 15:50:20 +0300

    services.hu-cafeteria-bot: use flakes

--- flake.nix
+++ flake.nix
@@ -4,13 +4,15 @@
   inputs = {
     agenix.url = "github:ryantm/agenix/main";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hu-cafeteria-bot.url = "github:hacettepeoyt/hu-cafeteria-bot/nix-flake";
   };
 
-  outputs = { self, agenix, nixpkgs }: {
+  outputs = { self, agenix, hu-cafeteria-bot, nixpkgs }: {
     nixosConfigurations."hacettepeoyt-vflower" = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
           agenix.nixosModules.default
+          hu-cafeteria-bot.nixosModules
           ./system.nix
         ];
     };
