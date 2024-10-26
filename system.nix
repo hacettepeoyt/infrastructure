e3cb66a65373ea2cac89cd1eb3ff36087fb66b8b
div72 <noreply@div72.xyz>
2024-10-26 14:51:04 +0300

    Revert "system: enable libvirtd"
    This reverts commit 8fe2becc83804e09f9432d925b0c69511ebdb2c1.

--- system.nix
+++ system.nix
@@ -52,8 +52,6 @@
     setSocketVariable = true;
   };
 
-  virtualisation.libvirtd.enable = true;
-
   security.acme = {
     acceptTerms = true;
     defaults.email = "sysadmin@lists.tlkg.org.tr";
@@ -104,7 +102,7 @@
       isNormalUser = true;
       hashedPasswordFile = config.age.secrets.passwd-div72.path;
       shell = pkgs.zsh;
-      extraGroups = [ "wheel" "libvirtd" ];
+      extraGroups = [ "wheel" ];
       packages = [ ]; # packages managed by home-manager
       openssh.authorizedKeys.keys = [ ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVb2l/23ykDnfhO5VrkCQaycfF9oCo1Jig/JeG86w//'' ];
     };
