1760a5e5810776df39bf3c2467fdc8eb2ab9b156
div72 <noreply@div72.xyz>
2025-10-20 13:47:39 +0300

    system: fix SSH agent auth for sudo
    Currently, PAM SSH logins fails with:
    
        PAM unable to dlopen(...): .../pam_ssh_agent_auth.so: undefined symbol: __multf3
    
    This comment patches pam_ssh_agent_auth.so to remedy that.
    
    See the issue in comment for more details.

--- system.nix
+++ system.nix
@@ -42,6 +42,18 @@
 
   security.pam.sshAgentAuth.enable = true;
   security.pam.services.sudo.sshAgentAuth = true;
+  # FIXME: Temporary fix for issue https://github.com/NixOS/nixpkgs/issues/386392.
+  # Courtesy of @psub - https://github.com/pSub/configs/blob/64a784b4b88d3579475294cb3e8797a7de68dddc/nixos/server/overlays/pam_ssh_agent_auth.nix
+  nixpkgs.overlays = [
+    (final:  prev :
+    {
+       pam_ssh_agent_auth = prev.pam_ssh_agent_auth.overrideAttrs (old: {
+        fixupPhase = ''
+          patchelf --add-needed ${prev.libgcc}/lib/libgcc_s.so.1 $out/libexec/pam_ssh_agent_auth.so
+        '';
+      });
+    })
+  ];
 
   programs.mosh.enable = true;
   programs.zsh.enable = true;
